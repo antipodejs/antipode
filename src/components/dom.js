f179f6e8c0d34d610df7ecc14a80c23b8803086d
Serge Alischuk <ut7cq@yahoo.com>
2016-04-30 22:55:30 +0300

    add few methods to work with DOM

---
+++ src/components/dom.js
@@ -0,0 +1,175 @@
+import platform from './platform';
+
+	/**
+	* dom
+	*/
+
+	const global = (function() {
+		return (new Function('return this')())
+	})();
+
+	const escape = (text) => {
+		return text !== null ? String(text).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') : '';
+	};
+
+	/**
+	* @private
+	*/
+	const getComputedStyle = (node) => {
+		if(platform.ie < 9 && node && node.currentStyle) {
+			//simple global.getComputedStyle polyfill for IE8
+			let computedStyle = utils.clone(node.currentStyle);
+			computedStyle.getPropertyValue = this._ie8GetComputedStyle;
+			computedStyle.setProperty = function() {
+				return node.currentStyle.setExpression.apply(node.currentStyle, arguments);
+			};
+			computedStyle.removeProperty = function() {
+				return node.currentStyle.removeAttribute.apply(node.currentStyle, arguments);
+			};
+			return computedStyle;
+		} else {
+			return global.getComputedStyle && node && global.getComputedStyle(node, null);
+		}
+	};
+
+	/**
+	* @private
+	*/
+	const getComputedStyleValue = (node, property, computedStyle) => {
+		let s   = computedStyle || getComputedStyle(node),
+			nIE = platform.ie;
+
+		s = s ? s.getPropertyValue(property) : null;
+
+		if (nIE) {
+			let oConversion = {
+				'thin'   : (nIE > 8 ? 2 : 1) + 'px',
+				'medium' : (nIE > 8 ? 4 : 3) + 'px',
+				'thick'  : (nIE > 8 ? 6 : 5) + 'px',
+				'none'   : '0'
+			};
+			if (typeof oConversion[s] != 'undefined') {
+				s = oConversion[s];
+			}
+
+			if (s == 'auto') {
+				switch (property) {
+				case 'width':
+					s = node.offsetWidth;
+					break;
+				case 'height':
+					s = node.offsetHeight;
+					break;
+				}
+			}
+		}
+
+		return s;
+	};
+
+	const hasClass = (node, s) => {
+		if (!node || !node.className) { return; }
+		return (' ' + node.className + ' ').indexOf(' ' + s + ' ') >= 0;
+	};
+
+	const addClass = (node, s) => {
+		if (node && !this.hasClass(node, s)) {
+			let ss = node.className;
+			node.className = (ss + (ss ? ' ' : '') + s);
+		}
+	};
+
+	const removeClass = (node, s) => {
+		if (node && this.hasClass(node, s)) {
+			let ss = node.className;
+			node.className = (' ' + ss + ' ').replace(' ' + s + ' ', ' ').slice(1, -1);
+		}
+	};
+
+	const setOpacity = (element, value) => {
+
+	    if (value == 1 || value === '') {
+	    	value = '';
+	    } else if (value < 0.00001) {
+	    	value = 0;
+	    }
+	    element.style.opacity = value;
+	    return element;
+	}
+
+	const setStyle = (element, styles) => {
+	 
+	    var elementStyle = element.style, match;
+
+	    if (typeof styles == "string") {
+			elementStyle.cssText += ';' + styles;
+			if (~styles.search('opacity')) {
+				let opacity = styles.match(/opacity:\s*(\d?\.?\d*)/)[1];
+				setOpacity(element, opacity);
+			}
+			return element;
+	    }
+
+	    for (let property in styles) {
+	    	if (property === 'opacity') {
+	    		setOpacity(element, styles[property]);
+	      	} else {
+	        	let value = styles[property];
+	        	if (property === 'float' || property === 'cssFloat') {
+	          		property = typeof(elementStyle.styleFloat) == "undefined"
+	          			? 'cssFloat' 
+	          			: 'styleFloat';
+	        	}
+	        	elementStyle[property] = value;
+	      	}
+	    }
+
+	    return element;
+	}
+
+	const camelize = (str) => {
+    	return str.replace(/-+(.)?/g, function(match, chr) {
+      		return chr ? chr.toUpperCase() : '';
+    	});
+  	}
+
+  	const capitalize = (str) => {
+    	return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+  	}
+
+  	const normalizeStyleName = (style) => {
+    	if (style === 'float' || style === 'styleFloat')
+      		return 'cssFloat';
+    	return camelize(style);
+  	}
+
+	const getStyle = (element, style) => {
+
+	    style = normalizeStyleName(style);
+
+	    let value = element.style[style];
+	    
+	    if (!value || value === 'auto') {
+	      var css = document.defaultView.getComputedStyle(element, null);
+	      value = css ? css[style] : null;
+	    }
+
+	    if (style === 'opacity') {
+	    	return value 
+	    		? parseFloat(value) 
+	    		: 1.0;
+	    }
+
+	    return value === 'auto' ? null : value;
+	}
+
+
+	export default {
+		escape,
+		getComputedStyleValue,
+		hasClass,
+		addClass,
+		removeClass,
+		setStyle,
+		getStyle
+	};
---
+++ src/components/platform.js
@@ -0,0 +1,70 @@
+const ua = navigator.userAgent;
+
+const ep = {};
+
+let platforms = [
+	// Android 4+ using Chrome
+	{platform: 'androidChrome', regex: /Android .* Chrome\/(\d+)[.\d]+/},
+	// Android 2 - 4
+	{platform: 'android', regex: /Android (\d+)/},
+	// Kindle Fire
+	// Force version to 2, (desktop mode does not list android version)
+	{platform: 'android', regex: /Silk\/1./, forceVersion: 2, extra: {silk: 1}},
+	// Kindle Fire HD (Silk versions 2 or 3)
+	// Force version to 4
+	{platform: 'android', regex: /Silk\/2./, forceVersion: 4, extra: {silk: 2}},
+	{platform: 'android', regex: /Silk\/3./, forceVersion: 4, extra: {silk: 3}},
+	// Windows Phone 7 - 8
+	{platform: 'windowsPhone', regex: /Windows Phone (?:OS )?(\d+)[.\d]+/},
+	// IE 8 - 10
+	{platform: 'ie', regex: /MSIE (\d+)/},
+	// IE 11
+	{platform: 'ie', regex: /Trident\/.*; rv:(\d+)/},
+	// iOS 3 - 5
+	// Apple likes to make this complicated
+	{platform: 'ios', regex: /iP(?:hone|ad;(?: U;)? CPU) OS (\d+)/},
+	// webOS 1 - 3
+	{platform: 'webos', regex: /(?:web|hpw)OS\/(\d+)/},
+	// webOS 4 / OpenWebOS
+	{platform: 'webos', regex: /WebAppManager|Isis|webOS\./, forceVersion: 4},
+	// Open webOS release LuneOS
+	{platform: 'webos', regex: /LuneOS/, forceVersion: 4, extra: {luneos: 1}},
+	// desktop Safari
+	{platform: 'safari', regex: /Version\/(\d+)[.\d]+\s+Safari/},
+	// desktop Chrome
+	{platform: 'chrome', regex: /Chrome\/(\d+)[.\d]+/},
+	// Firefox on Android
+	{platform: 'androidFirefox', regex: /Android;.*Firefox\/(\d+)/},
+	// FirefoxOS
+	{platform: 'firefoxOS', regex: /Mobile;.*Firefox\/(\d+)/},
+	// desktop Firefox
+	{platform: 'firefox', regex: /Firefox\/(\d+)/},
+	// Blackberry Playbook
+	{platform: 'blackberry', regex: /PlayBook/i, forceVersion: 2},
+	// Blackberry 10+
+	{platform: 'blackberry', regex: /BB1\d;.*Version\/(\d+\.\d+)/},
+	// Tizen
+	{platform: 'tizen', regex: /Tizen (\d+)/}
+];
+
+for (let i = 0, p, m, v; (p = platforms[i]); i++) {
+
+	m = p.regex.exec(ua);
+	if (m) {
+		if (p.forceVersion) {
+			v = p.forceVersion;
+		} else {
+			v = Number(m[1]);
+		}
+		ep[p.platform] = v;
+		if (p.extra) {
+			Object.assign(ep, p.extra);
+		}
+		ep.platformName = p.platform;
+		break;
+	}
+};
+
+ep['touch'] = Boolean('ontouchstart' in window);
+
+export default ep;
